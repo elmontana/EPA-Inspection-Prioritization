3246a9848cb7a0456ee9f9fcf606cbdb1aa4371b
dhruvmalik95 <dhruvmalik@berkeley.edu>
2020-11-03 17:24:25 -0800

    x

--- src/model_prep/aggregate_features.py
+++ src/model_prep/aggregate_features.py
@@ -116,7 +116,7 @@ def main(conn, config, cohort_table, to_table,
                     if metric == "datediff":
                         feature_name = f'{output_prefix}_days_since_{agg_column_name}'
                         #feature_str = f"min({metric}(day, {agg_column_name}, '{end_time}'::date)) as {feature_name}"
-                        feature_str = f"MIN( EXTRACT( DAY FROM '{end_time}'::date - {agg_column_name} )) as {feature_name}"
+                        feature_str = f"MIN('{end_time}'::date - {agg_column_name}) as {feature_name}"
 
                         feature_columns.append(feature_str)
 
