a515ada8b27c083326b699843807056ce2cc1f85
Erika Montana <erika.lowden@gmail.com>
2020-11-04 19:48:49 -0500

    implemented acs data loading

--- src/preprocessing/run.py
+++ src/preprocessing/run.py
@@ -3,6 +3,7 @@ import glob
 import sqlalchemy
 
 from ..utils.sql_utils import run_sql_from_file
+from load_acs import load_acs_data
 
 
 
@@ -10,10 +11,24 @@ dir_path = os.path.dirname(os.path.realpath(__file__))
 get_path = lambda s: os.path.join(dir_path, s)
 
 
-
 def run_data_loading(conn, mode, prefix):
     if mode == 'acs':
-        print('ACS data loading not implemented yet.')
+        #choose ACS variables to include and load them
+        variables = ['B01003_001E','B02001_002E', 'B02001_003E','B02001_005E','B19049_001E','B15003_002E','B15003_017E','B15003_022E','B15003_023E','B15003_025E']
+        variable_names = {'B01003_001E':'total_pop',
+                  'B02001_002E':'white_pop', 
+                  'B02001_003E':'black_pop',
+                  'B02001_005E':'asian_pop',
+                  'B19049_001E':'median_income',
+                  'B15003_002E':'edu_no_schooling',
+                  'B15003_017E':'edu_hsd',
+                  'B15003_022E':'edu_bs',
+                  'B15003_023E':'edu_ms',
+                  'B15003_025E':'edu_phd'}
+        table_name = prefix + "_acs_data"
+        survey = 'acs5'
+        year = 2013
+        load_acs_data(conn, variables, variable_names, table_name, survey, year)
 
 
 def run_step(conn, sql_files, prefix):
