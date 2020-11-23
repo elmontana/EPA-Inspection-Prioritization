e2ce19fa900d6136486d4ec57b57f5f1a2b58fc9
Jingyun Yang <jingyuny@usc.edu>
2020-11-23 14:37:29 -0500

    Implemented recall disparity plotting.

--- plot.py
+++ plot.py
@@ -2,13 +2,15 @@ import numpy as np
 import src.utils.data_utils as data_utils
 import src.utils.plot_utils as plot_utils
 
+import seaborn as sns
+from matplotlib import pyplot as plt
 from pathlib import Path
 from src.evaluate.model_selection import find_best_models
 
 
 
 def plot_results_over_time(
-    test_results_tables_prefix, 
+    test_results_tables_prefix,
     metrics=['precision_score_at_600', 'num_labeled_samples_at_600'],
     base_rates=['precision_score_at_1.0', None],
     figsize=(20, 10), save_dir='./plots/'):
@@ -24,12 +26,12 @@ def plot_results_over_time(
         - save_dir: directory where plots should be saved
     """
     plot_utils.plot_results_over_time(
-        test_results_tables_prefix, 
+        test_results_tables_prefix,
         metrics=metrics, base_rates=base_rates, save_dir=save_dir)
 
 
 def plot_best_results_over_time(
-    test_results_tables_prefix, 
+    test_results_tables_prefix,
     metric='precision_score_at_600',
     other_metrics=['num_labeled_samples_at_600'],
     n=5,
@@ -51,7 +53,7 @@ def plot_best_results_over_time(
     best_model_idx = find_best_models(test_results_tables_prefix, metric=metric, n=n)
     plot_utils.plot_results_over_time(
         test_results_tables_prefix,
-        metrics=([metric] + other_metrics), base_rates=base_rates, 
+        metrics=([metric] + other_metrics), base_rates=base_rates,
         model_idx=best_model_idx, save_dir=save_dir)
 
 
@@ -59,7 +61,7 @@ def plot_precision_recall_curves(results_table_name, save_dir='./plots/'):
     """
     Plot precision recall curves for each model in a set of results.
 
-    Arguments: 
+    Arguments:
         - results_table_name: name of results table
         - save_dir: directory where plots should be saved
     """
@@ -71,12 +73,12 @@ def plot_precision_recall_curves(results_table_name, save_dir='./plots/'):
 
 
 def plot_best_precision_recall_curves(
-    results_table_name, 
+    results_table_name,
     metric='precision_score_at_600', n=5, save_dir='./plots/'):
     """
     Plot precision recall curves for the best models.
 
-    Arguments: 
+    Arguments:
         - results_table_name: name of results table
         - metric: the metric to use for selecting best models
         - n: number of models to plot curves for
@@ -88,18 +90,88 @@ def plot_best_precision_recall_curves(
 
     if not results_table_name.startswith('results.'):
         results_table_name = f'results.{results_table_name}'
-    
+
     results_df = data_utils.get_table(results_table_name)
     results_df = results_df.iloc[best_model_idx]
     plot_utils.plot_pr_at_k(results_df, Path(save_dir) / 'curve')
 
 
+def plot_recall_disparity_over_groups(
+    results_table_name,
+    feature_name='county_population', feature_threshold=200_000,
+    metric='precision_score_at_600', save_dir='./plots/'):
+    """
+    Plot recall disparity scatter plot over groups.
+
+    Arguments:
+        - results_table_name: name of results table
+        - feature_name: feature name that is used to identify groups
+        - feature_threshold: threshold to split the data to two groups
+        - metric: the metric to use for metric axis
+        - save_dir: directory where plots should be saved
+    """
+    metric_k = metric.split('_at_')[-1]
+    results_table_prefix = results_table_name.split('_test_results')[0]
+    feature_table_name = f'experiments.{results_table_prefix}_test_features'
+    label_table_name = f'experiments.{results_table_prefix}_test_labels'
+
+    results_df = data_utils.get_table(f'results.{results_table_name}')
+    feature_df = data_utils.get_table(feature_table_name)
+    group_ids = (feature_df[feature_name].to_numpy() > feature_threshold).astype(int)
+    label_df = data_utils.get_table(label_table_name)
+
+    num_models = len(results_df)
+    model_classes = list(set(results_df['model_class']))
+    model_metrics = results_df[metric].to_numpy()
+    model_recall_disparities = []
+    for i in range(num_models):
+        prediction_table_name = f'predictions.{results_table_prefix}_test_model_{i}_pred_at_{metric_k}'
+        prediction_df = data_utils.get_table(prediction_table_name)
+        prediction_df['group_ids'] = group_ids
+        combined_df = prediction_df.join(label_df.set_index('entity_id'), on='entity_id').dropna()
+        predictions = combined_df['Prediction'].to_numpy()
+        labels = combined_df['label'].to_numpy()
+        gids = combined_df['group_ids'].to_numpy()
+
+        # get group 0 recall
+        tp0 = np.sum((predictions[gids == 0] == labels[gids == 0]) * (labels[gids == 0] == 1))
+        pc0 = np.sum(labels[gids == 0])
+        recall0 = tp0 / pc0
+
+        # get group 1 recall
+        tp1 = np.sum((predictions[gids == 1] == labels[gids == 1]) * (labels[gids == 1] == 1))
+        pc1 = np.sum(labels[gids == 0])
+        recall1 = tp1 / pc1
+
+        model_recall_disparities.append(np.abs(recall0 - recall1))
+    model_recall_disparities = np.array(model_recall_disparities)
+
+    # prepare colors for the scatter plot
+    vis_rgb = sns.color_palette("Set2", len(model_classes))
+    colormap = dict(zip(list(range(len(model_classes))), vis_rgb))
+
+    plt.clf()
+    for i in range(len(model_classes)):
+        model_indices = [k for k in range(len(model_classes)) \
+                         if results_df.iloc[k]['model_class'] == model_classes[i]]
+        plt.scatter(model_metrics[model_indices], model_recall_disparities[model_indices],
+                    c=[colormap[i]], s=4)
+    plt.xlabel(metric)
+    plt.ylabel(f'Recall Disparity for Different [{feature_name}] Groups')
+    plt.xlim(0, 0.25)
+    plt.ylim(0, 1)
+    plt.legend(model_classes)
+    plt.tight_layout()
+    plt.savefig(Path(save_dir) / 'model_disparity.pdf')
+
+
 
 if __name__ == '__main__':
-    # So that every time we want to plot something, 
+    # So that every time we want to plot something,
     # we don't have to run main.py and spend an hour training models;
     # instead just use the results that are already in the database.
-    
+
+    '''
     print('Plotting precision over time ...')
     test_results_tables_prefix = 'i_v1_model_grid_201115015235'
     plot_results_over_time(test_results_tables_prefix)
@@ -111,3 +183,6 @@ if __name__ == '__main__':
     print('Plotting precision recall curves for best 5 models over time ...')
     results_table_name = 'i_v1_model_grid_201115015235_160101_test_results'
     plot_precision_recall_curves(results_table_name)
+    '''
+
+    plot_recall_disparity_over_groups('i_v1_test_run_201113235700_160101_test_results')
